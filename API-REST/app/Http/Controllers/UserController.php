2e7dc1fc15cea47631829c05bd8fda38bf45d685
Cristhian-Valenci <cristhianvalenci22@gmail.com>
2025-12-04 11:46:42 +0100

    TDD GREEN: cannot list when users no exsists

--- API-REST/app/Http/Controllers/UserController.php
+++ API-REST/app/Http/Controllers/UserController.php
@@ -16,6 +16,10 @@ class UserController extends Controller
     {
         $users = User::all();
 
+        if($users->isEmpty()) {
+            return response()->json([],204);
+        }
+
         return response()->json($users,200);
     }
 
