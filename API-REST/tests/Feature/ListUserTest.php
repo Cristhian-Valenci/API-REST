78ed3f79eb7ed48c01f58f3da3b489e4bc3e6877
Cristhian-Valenci <cristhianvalenci22@gmail.com>
2025-12-04 10:43:18 +0100

    TDD GREEN: implement logic to list users

--- API-REST/tests/Feature/ListUserTest.php
+++ API-REST/tests/Feature/ListUserTest.php
@@ -5,12 +5,15 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use app\Http\Controllers\UserController;
+use App\Http\Requests\UserRequest;
+use App\Models\User;
+
 
 class ListUserTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
+    use RefreshDatabase;
+    
     public function test_user_can_list(): void
     {
       $users = User::factory()->count(2)->create();
