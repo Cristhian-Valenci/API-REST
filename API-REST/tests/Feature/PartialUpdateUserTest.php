fefa3f2d683839598742ee1b6071bd18f118b9d6
Cristhian-Valenci <cristhianvalenci22@gmail.com>
2025-12-06 15:46:32 +0100

    Test user can patch password only

--- API-REST/tests/Feature/PartialUpdateUserTest.php
+++ API-REST/tests/Feature/PartialUpdateUserTest.php
@@ -64,4 +64,22 @@ class PartialUpdateUserTest extends TestCase
         ]);
     }
 
+
+    public function test_user_can_patch_password_only(): void 
+    {
+        $user = User::factory()->create();
+
+        $data = [
+           'password' => 'nuevaclave123',
+           'password_confirmation' => 'nuevaclave123'
+        ];
+ 
+        $response = $this->patchJson("/api/users/{$user->id}", $data);
+
+        $response->assertStatus(200);
+
+        $this->assertTrue(Hash::check('nuevaclave123', $user->fresh()->password));
+
+    }
+
 }
