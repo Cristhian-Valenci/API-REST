bf4f0696704ae0e6a8a1ab4401f06ae94a57cb54
Cristhian-Valenci <cristhianvalenci22@gmail.com>
2025-12-06 16:09:05 +0100

    TDD RED: Create test to delete a User and add method to do

---
+++ API-REST/tests/Feature/DeleteUserTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class DeleteUserTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_user_can_be_deleted(): void
+    {
+     
+        $user = User::factory()->create();
+
+        $response = $this->deleteJson("/api/users/{$user->id}");
+
+        $response->assertStatus(204);
+
+        $this->assertDatabaseMissing('users', [
+            'id' => $user->id,
+        ]);
+    }
+}
