9a496ffe687df828e3ce54e60b78b67aa63583da
Cristhian-Valenci <cristhianvalenci22@gmail.com>
2025-12-06 15:40:06 +0100

    Add test to patch email only

--- API-REST/tests/Feature/PartialUpdateUserTest.php
+++ API-REST/tests/Feature/PartialUpdateUserTest.php
@@ -11,31 +11,57 @@ use Illuminate\Support\Facades\Hash;
 class PartialUpdateUserTest extends TestCase
 {
     use RefreshDatabase;
-    
+
     public function test_user_can_patch_name_only(): void
     {
        $user = User::factory()->create([
-          'name' => 'Nombre original',
+          'name' => 'Original name',
           'email' => 'original@email.com'
         ]);
 
         $data = [
-           'name' => 'Nombre Actualizado'
+           'name' => 'Update name'
         ];
 
         $response = $this->patchJson("/api/users/{$user->id}", $data);
 
         $response->assertStatus(200)
                  ->assertJsonFragment([
-                   'name' => 'Nombre Actualizado',
+                   'name' => 'Update name',
                    'email' => 'original@email.com',
                 ]);
 
         $this->assertDatabaseHas('users', [
            'id' => $user->id,
-           'name' => 'Nombre Actualizado',
+           'name' => 'Update name',
            'email' => 'original@email.com',
         ]);
-}
+    }
+
+    public function test_user_can_patch_email_only(): void
+    {
+       $user = User::factory()->create([
+          'name' => 'Original name',
+          'email' => 'original@email.com'
+        ]);
+
+        $data = [
+           'email' => 'update@email.com'
+        ];
+
+        $response = $this->patchJson("/api/users/{$user->id}", $data);
+
+        $response->assertStatus(200)
+                 ->assertJsonFragment([
+                   'name' => 'Original name',
+                   'email' => 'update@email.com',
+                ]);
+
+        $this->assertDatabaseHas('users', [
+           'id' => $user->id,
+           'name' => 'Original name',
+           'email' => 'update@email.com',
+        ]);
+    }
 
 }
