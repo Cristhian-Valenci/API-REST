3402a784f9f880c422b6ad9cdff03315e7be023c
Cristhian-Valenci <cristhianvalenci22@gmail.com>
2025-12-10 10:26:11 +0100

    Add test when ingredient cannot update

--- API-REST/tests/Feature/IngredientUpdateTest.php
+++ API-REST/tests/Feature/IngredientUpdateTest.php
@@ -25,4 +25,60 @@ class IngredientUpdateTest extends TestCase
                 ->assertJsonFragment(['name' => 'Rum']);
     }
 
+        public function test_update_nonexistent_ingredient()
+    {
+        $nonExistentId = Ingredient::max('id') + 1;
+
+        $response = $this->putJson("/api/ingredients/{$nonExistentId}", [
+            'name' => 'NonExistenIngredient'
+        ]);
+
+
+        $response->assertStatus(404)
+                ->assertJson([
+                    'message' => 'Ingredient not found'
+                ]);
+    }
+
+        public function test_update_without_name()
+    {
+        $ingredient = Ingredient::create(['name' => 'Vodka']);
+
+        $response = $this->putJson("/api/ingredients/{$ingredient->id}", [
+            'name' => ''
+        ]);
+
+        $response->assertStatus(422)
+                ->assertJsonValidationErrors('name');
+    }
+
+        public function test_update_with_duplicate_name()
+    {
+        Ingredient::create(['name' => 'Vodka']);
+        $ingredient2 = Ingredient::create(['name' => 'Tequila']);
+
+        $response = $this->putJson("/api/ingredients/{$ingredient2->id}", [
+            'name' => 'Vodka'
+        ]);
+
+        $response->assertStatus(422)
+                ->assertJsonValidationErrors('name');
+    }
+
+        public function test_update_with_invalid_characters()
+    {
+        $ingredient = Ingredient::create(['name' => 'Vodka']);
+
+        $response = $this->putJson("/api/ingredients/{$ingredient->id}", [
+            'name' => 'Vodka123!!'
+        ]);
+
+        $response->assertStatus(422)
+                ->assertJsonValidationErrors('name');
+    }
+
+
+
+
+
 }
