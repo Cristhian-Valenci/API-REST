af4ec681b80f3158972eeb93d9f03e0de96e2723
Cristhian-Valenci <cristhianvalenci22@gmail.com>
2025-12-11 12:33:14 +0100

    Create policies and TDD RED: to test owner can update ingredient

---
+++ API-REST/tests/Feature/IngredientUpdateAuthorizationTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use App\Models\Ingredient;
+use App\Models\Cocktail;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class IngredientUpdateTest extends TestCase
+{
+    use RefreshDatabase;
+
+    
+    public function test_owner_can_update_ingredient()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        $ingredient = Ingredient::factory()->create([
+            'user_id' => $user->id,
+            'name' => 'Vodka',
+        ]);
+
+        $response = $this->putJson("/api/ingredients/{$ingredient->id}", [
+            'name' => 'Tequila',
+        ]);
+
+        $response->assertStatus(200)
+                 ->assertJsonFragment(['name' => 'Tequila']);
+
+        $this->assertDatabaseHas('ingredients', [
+            'id' => $ingredient->id,
+            'name' => 'Tequila',
+            'user_id' => $user->id,
+        ]);
+    }
+}
