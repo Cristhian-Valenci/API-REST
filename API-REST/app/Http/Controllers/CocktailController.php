1e8a04a6bc7064710a03fc2d65309c65350b2b70
Cristhian-Valenci <cristhianvalenci22@gmail.com>
2025-12-17 17:57:47 +0100

    TDD GREEN: Update store function on CocktailController

--- API-REST/app/Http/Controllers/CocktailController.php
+++ API-REST/app/Http/Controllers/CocktailController.php
@@ -3,11 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Cocktail;
 
 class CocktailController extends Controller
 {
     public function store(Request $request)
     {
-        return response()->json([], 201);
+        $cocktail = Cocktail::create([
+            'name' => $request->name,
+            'description' => $request->description,
+            'elaboration_method' => $request->elaboration_method,
+            'user_id' => auth()->id(),
+        ]);
+
+        if ($request->has('ingredients')) {
+            foreach ($request->ingredients as $ingredient) {
+                $cocktail->ingredients()->attach(
+                    $ingredient['ingredient_id'],
+                    [
+                        'amount' => $ingredient['amount'],
+                        'unit' => $ingredient['unit'],
+                    ]
+                );
+            }
+        }
+
+        return response()->json([
+            'id' => $cocktail->id,
+            'name' => $cocktail->name,
+        ], 201);
+
     }
 }
