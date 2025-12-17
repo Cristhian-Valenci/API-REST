1312ce967866ab37983e75d2113ff9a676d55d7e
Cristhian-Valenci <cristhianvalenci22@gmail.com>
2025-12-17 13:36:42 +0100

    TDD GREEN: Add test authenticated user can create cocktail

--- API-REST/tests/Feature/CocktailCreateTest.php
+++ API-REST/tests/Feature/CocktailCreateTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
 
 class CocktailCreateTest extends TestCase
 {
@@ -13,9 +14,9 @@ class CocktailCreateTest extends TestCase
     public function test_guest_cannot_create_cocktail(): void
     {
         $payload = [
-            'nombre' => 'Margarita',
-            'descripcion' => 'Classic mexican cocktail',
-            'metodo_elaboracion' => 'Shake with ice and serve on martini glass',
+            'name' => 'Margarita',
+            'description' => 'Classic mexican cocktail',
+            'elaboration_method' => 'Shake with ice and serve on martini glass',
             'ingredients' => []
         ];
 
@@ -23,4 +24,23 @@ class CocktailCreateTest extends TestCase
 
         $response->assertStatus(401);
     }
+
+    public function test_authenticated_user_can_create_cocktail()
+    {
+        $user = User::factory()->create();
+
+        $payload = [
+            'name' => 'Margarita',
+            'description' => 'Classic cocktail',
+            'elaboration_method' => 'Shake with ice',
+            'ingredients' => []
+        ];
+
+        $response = $this->actingAs($user, 'api')
+                        ->postJson('/api/cocktails', $payload);
+
+        $response->assertStatus(201);
+    }
+
+
 }
