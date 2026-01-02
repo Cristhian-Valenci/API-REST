6e566cc505c9868387c552ffe99efe72735cd071
Cristhian-Valenci <cristhianvalenci22@gmail.com>
2026-01-02 18:56:46 +0100

    TDD RED: Create CocktailShowTest with test show returns cocktail with ingredients

---
+++ API-REST/tests/Feature/CocktailShowTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\Models\Cocktail;
+
+class CocktailShowTest extends TestCase
+{
+    use RefreshDatabase;
+
+   
+    public function test_show_returns_cocktail_with_ingredients()
+    {
+        $cocktail = Cocktail::factory()->withIngredients()->create();
+
+        $response = $this->getJson("/api/cocktails/{$cocktail->id}");
+
+        $response->assertStatus(200);
+
+        $cocktailJson = $response->json();
+
+        $this->assertArrayHasKey('id', $cocktailJson);
+        $this->assertArrayHasKey('name', $cocktailJson);
+        $this->assertArrayHasKey('ingredients', $cocktailJson);
+        $this->assertNotEmpty($cocktailJson['ingredients']);
+
+        foreach ($cocktailJson['ingredients'] as $ingredient) {
+            $this->assertArrayHasKey('id', $ingredient);
+            $this->assertArrayHasKey('name', $ingredient);
+            $this->assertArrayHasKey('amount', $ingredient);
+            $this->assertArrayHasKey('unit', $ingredient);
+        }
+    }
+}
